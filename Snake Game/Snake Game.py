3c0a0ecda2435b3971f9d966db6944988c0c59d6
Azmeerpirani <azmeerpirani3@gmail.com>
2026-09-20 15:26:19 +0530

    added a logic to change the colors according to the amount of score achieved, also added a small start message and spacebar to start the game, changed the color of the head from the rest of the body of snake

--- Snake Game/Snake Game.py
+++ Snake Game/Snake Game.py
@@ -9,18 +9,33 @@ y=300
 move_x=20
 move_y=0
 
+bg_colors = [
+    (0, 0, 0),
+    (10, 10, 30),
+    (20, 10, 10),
+    (10, 25, 10),
+    (25, 20, 5)
+]
+
 food_x=random.randrange(0,800,20)
 food_y=random.randrange(0,600,20)
 
 score=0
 font=pygame.font.SysFont(None,30)
 
+body_x=[]
+body_y=[]
+length=1
+
+started = False
 running=True
 while running==True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running= False
+            running = False
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                started = True
             if event.key == pygame.K_UP:
                 move_x=0
                 move_y=-20
@@ -33,13 +48,15 @@ while running==True:
             if event.key == pygame.K_LEFT:
                 move_x=-20
                 move_y=0
-    x=x+move_x
-    y=y+move_y
+
+    if started == True:         
+        x=x+move_x
+        y=y+move_y
 
     if x>=800:
         x=0
     if x<0:
-        x=780
+        x=780    
     if y>=600:
         y=0
     if y<0:
@@ -47,14 +64,47 @@ while running==True:
 
     if x==food_x and y==food_y:
         score +=1
+        length=length+1
         food_x = random.randrange(0,800,20)
         food_y = random.randrange(0,600,20)
+
+    body_x.append(x)
+    body_y.append(y)
+    if len(body_x)>length:
+        del body_x[0]
+        del body_y[0]
         
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen,(255,0,0),(food_x,food_y,20,20))
-    pygame.draw.rect(screen,(0,255,0),(x,(y-1),20,20))
-    text = font.render("Score"+str(score),True,(255,255,255))
-    screen.blit(text,(10,10))
+    background = bg_colors[(score//5)%len(bg_colors)]
+    screen.fill(background)
+
+    
+
+    for grid_x in range(0, 800, 20):
+        pygame.draw.line(screen, (35, 35, 35), (grid_x, 0), (grid_x, 600), 1)
+
+    for grid_y in range(0, 800, 20):
+        pygame.draw.line(screen, (35, 35, 35), (0, grid_y), (800, grid_y), 1)
+
+    pygame.draw.rect(screen,(150,0,0),(food_x,food_y,20,20))
+    pygame.draw.rect(screen,(0,255,100),(x,(y-1),20,20))
+    for b in range(len(body_x)):
+        pygame.draw.rect(screen,(0,150,0),(body_x[b],body_y[b],19,19))
+    score_text = font.render("Score: "+str(score), True, (255,255,255))
+    screen.blit(score_text,(10,10))
+    length_text = font.render("Length: " + str(score), True, (255, 255, 255))
+    screen.blit(length_text, (120, 10))
+
+    if not started:
+        start_text = font.render(
+            "PRESS SPACE TO START",
+            True,
+            (255, 255, 255)
+        )
+
+        start_rect = start_text.get_rect(center = (400, 300))
+
+        screen.blit(start_text, start_rect)
+
     pygame.display.flip()
 
     pygame.time.delay(150)
